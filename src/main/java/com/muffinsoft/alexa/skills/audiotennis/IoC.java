c986331c09b418c8b3d9ca1c889fa2c130e4a235
metalleveller <oleksandr.solodovnikov@gmail.com>
2018-12-10 15:08:40 +0200

    add sound mapping

--- src/main/java/com/muffinsoft/alexa/skills/audiotennis/IoC.java
+++ src/main/java/com/muffinsoft/alexa/skills/audiotennis/IoC.java
@@ -12,6 +12,7 @@ import com.muffinsoft.alexa.skills.audiotennis.content.CharactersManager;
 import com.muffinsoft.alexa.skills.audiotennis.content.GeneralActivityPhraseManager;
 import com.muffinsoft.alexa.skills.audiotennis.content.ProgressManager;
 import com.muffinsoft.alexa.skills.audiotennis.content.RegularPhraseManager;
+import com.muffinsoft.alexa.skills.audiotennis.content.SoundsManager;
 import com.muffinsoft.alexa.skills.audiotennis.content.UserReplyManager;
 import com.muffinsoft.alexa.skills.audiotennis.models.DictionaryManager;
 import com.muffinsoft.alexa.skills.audiotennis.models.PhraseDependencyContainer;
@@ -33,6 +34,7 @@ public class IoC {
     private static final CharactersManager CHARACTERS_MANAGER;
     private static final ActivityManager ACTIVITY_MANAGER;
     private static final DictionaryManager DICTIONARY_MANAGER;
+    private static final SoundsManager SOUNDS_MANAGER;
 
     static {
         REGULAR_PHRASE_MANAGER = new RegularPhraseManager("phrases/en-US.json");
@@ -41,7 +43,8 @@ public class IoC {
         ALIAS_MANAGER = new AliasManager("settings/aliases.json");
         PROGRESS_MANAGER = new ProgressManager("settings/defaults.json");
         CHARACTERS_MANAGER = new CharactersManager("phrases/characters.json");
-        BASE_DIALOG_TRANSLATOR = new BaseDialogTranslator(CHARACTERS_MANAGER.getContainer());
+        SOUNDS_MANAGER = new SoundsManager("phrases/sounds.json");
+        BASE_DIALOG_TRANSLATOR = new BaseDialogTranslator(CHARACTERS_MANAGER.getContainer(), SOUNDS_MANAGER.getContainer());
         ACTIVITIES_PHRASE_MANAGER = new ActivitiesPhraseManager();
         GENERAL_ACTIVITY_PHRASE_MANAGER = new GeneralActivityPhraseManager();
         DICTIONARY_MANAGER = new DictionaryManager();
---
+++ src/main/java/com/muffinsoft/alexa/skills/audiotennis/content/SoundsManager.java
@@ -0,0 +1,10 @@
+package com.muffinsoft.alexa.skills.audiotennis.content;
+
+import com.muffinsoft.alexa.sdk.content.BaseContentManager;
+
+public class SoundsManager extends BaseContentManager<String> {
+
+    public SoundsManager(String path) {
+        super(path);
+    }
+}
