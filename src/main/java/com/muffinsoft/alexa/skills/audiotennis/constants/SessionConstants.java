f25903823efb7a453481ce05e59b890fb4bd0993
metalleveller <oleksandr.solodovnikov@gmail.com>
2019-09-24 14:50:54 +0300

    add description of the flags

--- src/main/java/com/muffinsoft/alexa/skills/audiotennis/constants/SessionConstants.java
+++ src/main/java/com/muffinsoft/alexa/skills/audiotennis/constants/SessionConstants.java
@@ -2,11 +2,24 @@ package com.muffinsoft.alexa.skills.audiotennis.constants;
 
 public class SessionConstants extends com.muffinsoft.alexa.sdk.constants.SessionConstants {
 
-    public static final String SWITCH_ACTIVITY_STEP = "switchActivityStep";
+    // chose direct activity on the start or from menu
+    // handles only activity types or returns to the menu
     public static final String SELECT_ACTIVITY_STEP = "selectActivityStep";
+
+    //same as above, but also handles negative and positive replies
+    // positive will redirects to the possible activity
+    public static final String SWITCH_ACTIVITY_STEP = "switchActivityStep";
+
     public static final String BLOCKED_ACTIVITY_CALL = "blockedActivityCall";
+
+    // when activity just unlocked proposal to play in this activity
+    // positive reply changes activity to new one, negative will continue old activity
     public static final String SWITCH_UNLOCK_ACTIVITY_STEP = "switchUnlockActivityStep";
+
     public static final String EXIT_FROM_HELP = "exitFromHelp";
+
+    //positive reply redirects to the random opened activity
+    // negative will proceed current activity
     public static final String ASK_RANDOM_SWITCH_ACTIVITY_STEP = "askRandomSwitchActivityStep";
     public static final String EXIT_FROM_ONE_POSSIBLE_ACTIVITY = "exitFromOnePossibleActivity";
     public static final String LAST_PURCHASE_ATTEMPT_ON = "lastPurchaseAttemptOn";
